edd5694630801c411c3226a08173f4bf6b4e8e28
VanChester13 <lifesnotfair@mail.ru>
2024-03-12 14:20:03 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import posts from "./db";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Posts from "./pages/Posts/Posts";
 import DetailPost from "./pages/DetailPost/DetailPost";
 
 function App(): React.ReactElement {
   return (
     <div id="main-container">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Posts posts={posts} />} />
           <Route path="/posts/:id" element={<DetailPost />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
